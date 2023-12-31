c04b441886e820361f3ec2253ad53c3fbf7a9c81
Ryan Cox <39605026+ryancox23@users.noreply.github.com>
2023-12-31 13:00:10 -0500

    Core Restaurant App Components

---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { SvgXml } from "react-native-svg";
+import { Text } from "../../../components/typography/text.component";
+import {
+  Open,
+  Address,
+  Icon,
+  RestaurantCard,
+  RestaurantCardCover,
+  Info,
+  Section,
+  Rating,
+  SectionEnd,
+} from "./restaurant-info-card.styles";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 Random St",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily = true,
+  } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
+        <Text variant="label">{name}</Text>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} height={20} width={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="error">CLOSED TEMPORARILY</Text>
+            )}
+            <Spacer position="left" size="large">
+              {isOpenNow && <Open xml={open} height={20} width={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <Icon source={{ uri: icon }} />
+            </Spacer>
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
+    </RestaurantCard>
+  );
+};
